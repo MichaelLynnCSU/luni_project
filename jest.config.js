d8a03d1c871fcf3ff7f1b05c89a180708bd84e1e
michael.lynn <hailviral101@yahoo.com>
2024-06-21 13:59:26 -0600

    scraper file
    helloi

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
-// jest.config.js
 module.exports = {
   testMatch: [
     '**/__tests__/**/*.[jt]s?(x)',
@@ -8,17 +7,20 @@ module.exports = {
   testPathIgnorePatterns: [
     '\\\\node_modules\\\\'
   ],
-  roots: ["<rootDir>/src"], // Adjust this based on your frontend directory
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"], // Adjust path as per your setup file location
+  roots: ["<rootDir>/src"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    // Add a mapping for canvas.node to the actual path in node_modules
+    '^canvas$': '<rootDir>/node_modules/canvas/build/Release/canvas.node',
+    // Adjust the path based on where canvas.node is located in your node_modules
   },
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest"
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!(@ant-design|antd|other-package)/)" // Adjust as per your needs
-  ]
-  // other Jest configurations...
+    "/node_modules/(?!(@ant-design|antd|other-package)/)"
+  ],
+  // Other Jest configurations...
 };
