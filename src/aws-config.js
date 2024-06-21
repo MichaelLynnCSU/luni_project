5b116b50e5fa088d57de0526e1d042e0575763d9
michael.lynn <hailviral101@yahoo.com>
2024-06-21 17:17:10 -0600

    remove config
    remove config

--- src/aws-config.js
+++
@@ -1,9 +0,0 @@
-import AWS from 'aws-sdk';
-
-AWS.config.update({
-    accessKeyId: 'AKIAWYBABBK5OAHAOVUX',
-    secretAccessKey: '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',
-    region: 'us-east-1',
-});
-
-export default AWS;
